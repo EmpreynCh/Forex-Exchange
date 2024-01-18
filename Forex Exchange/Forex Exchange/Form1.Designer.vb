7ea5c7cb406349e80433e05d4b57b6dd8ea44053
EmpreynCh <emprhaimquiambao03@gmail.com>
2024-01-18 22:08:18 +0800

    vbnet project sheeshhh

---
+++ Forex Exchange/Forex Exchange/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Com1 = New System.Windows.Forms.ComboBox()
+        Me.Com2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Pic1 = New System.Windows.Forms.PictureBox()
+        Me.Pic2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Com1
+        '
+        Me.Com1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Com1.Font = New System.Drawing.Font("Segoe UI Emoji", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Com1.FormattingEnabled = True
+        Me.Com1.Items.AddRange(New Object() {"USD $", "EUR  €", "GBP £", "JPY ¥", "PHP ₱", "ر.سSAR", "CYN ¥"})
+        Me.Com1.Location = New System.Drawing.Point(141, 116)
+        Me.Com1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Com1.Name = "Com1"
+        Me.Com1.Size = New System.Drawing.Size(200, 44)
+        Me.Com1.TabIndex = 0
+        '
+        'Com2
+        '
+        Me.Com2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Com2.Font = New System.Drawing.Font("Segoe UI Emoji", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Com2.FormattingEnabled = True
+        Me.Com2.Items.AddRange(New Object() {"USD $", "EUR  €", "GBP £", "JPY ¥", "PHP ₱", "ر.سSAR", "CYN ¥"})
+        Me.Com2.Location = New System.Drawing.Point(725, 127)
+        Me.Com2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Com2.Name = "Com2"
+        Me.Com2.Size = New System.Drawing.Size(200, 44)
+        Me.Com2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Gold
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(141, 445)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(200, 42)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Gold
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(725, 445)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(200, 42)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Pic1
+        '
+        Me.Pic1.BackColor = System.Drawing.Color.Transparent
+        Me.Pic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Pic1.Location = New System.Drawing.Point(51, 179)
+        Me.Pic1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Pic1.Name = "Pic1"
+        Me.Pic1.Size = New System.Drawing.Size(440, 233)
+        Me.Pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Pic1.TabIndex = 6
+        Me.Pic1.TabStop = False
+        '
+        'Pic2
+        '
+        Me.Pic2.BackColor = System.Drawing.Color.Transparent
+        Me.Pic2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Pic2.Location = New System.Drawing.Point(596, 179)
+        Me.Pic2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Pic2.Name = "Pic2"
+        Me.Pic2.Size = New System.Drawing.Size(422, 233)
+        Me.Pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Pic2.TabIndex = 5
+        Me.Pic2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(516, 451)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(65, 61)
+        Me.Button1.TabIndex = 2
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Corbel", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Crimson
+        Me.Label1.Location = New System.Drawing.Point(501, 416)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 31)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Convert"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Label2.Font = New System.Drawing.Font("Corbel", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Label2.Location = New System.Drawing.Point(200, 81)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 31)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "From"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Corbel", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Label3.Location = New System.Drawing.Point(801, 81)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 31)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "To"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Monotype Corsiva", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Lime
+        Me.Label4.Location = New System.Drawing.Point(353, 20)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(366, 72)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Forex Currency"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1076, 554)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Pic1)
+        Me.Controls.Add(Me.Pic2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Com2)
+        Me.Controls.Add(Me.Com1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Forex Exchange"
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Com1 As ComboBox
+    Friend WithEvents Com2 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Pic2 As PictureBox
+    Friend WithEvents Pic1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Forex Exchange/Forex Exchange/Form1.vb
@@ -0,0 +1,192 @@
+﻿Public Class Form1
+    Dim Num1 As Decimal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Pic1.BorderStyle = BorderStyle.FixedSingle
+        Pic2.BorderStyle = BorderStyle.FixedSingle
+    End Sub
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        Dim DecimalSeparator As String = Application.CurrentCulture.NumberFormat.NumberDecimalSeparator
+        e.Handled = Not (Char.IsDigit(e.KeyChar) Or
+                     Asc(e.KeyChar) = 8 Or
+                     (e.KeyChar = DecimalSeparator And sender.Text.IndexOf(DecimalSeparator) = -1))
+    End Sub
+    Private Sub CB1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Com1.SelectedIndexChanged
+        If Com1.SelectedIndex = 0 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\America.gif")
+        ElseIf Com1.SelectedIndex = 1 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\Europe.gif")
+        ElseIf Com1.SelectedIndex = 2 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\US.gif")
+        ElseIf Com1.SelectedIndex = 3 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\Japan.gif")
+        ElseIf Com1.SelectedIndex = 4 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\Philippines.gif")
+        ElseIf Com1.SelectedIndex = 5 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\Arab.gif")
+        ElseIf Com1.SelectedIndex = 6 Then
+            Pic1.Image = Image.FromFile(Application.StartupPath + "\Pics\China.gif")
+        End If
+    End Sub
+    Private Sub CB2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Com2.SelectedIndexChanged
+        If Com2.SelectedIndex = 0 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\America.gif")
+        ElseIf Com2.SelectedIndex = 1 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\Eunion.gif")
+        ElseIf Com2.SelectedIndex = 2 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\ukay.gif")
+        ElseIf Com2.SelectedIndex = 3 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\jap.gif")
+        ElseIf Com2.SelectedIndex = 4 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\Philippines.gif")
+        ElseIf Com2.SelectedIndex = 5 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\Arab.gif")
+        ElseIf Com2.SelectedIndex = 6 Then
+            Pic2.Image = Image.FromFile(Application.StartupPath + "\Pics\Chine.gif")
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Clear()
+        If TextBox1.Text = "" Then
+            MessageBox.Show("Enter a number to convert!")
+            Num1 = 0
+        Else
+            Num1 = TextBox1.Text
+        End If
+        'USD'
+        If Com1.SelectedIndex = 0 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Num1 * 1)
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.95, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.82, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 149.39, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 56.81, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 3.75, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 7.23, 2))
+            End If
+            'EURO'
+        ElseIf Com1.SelectedIndex = 1 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1.05, 2))
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.87, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 157.28, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 59.82, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 3.95, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 7.56, 2))
+            End If
+            'GBP'
+        ElseIf Com1.SelectedIndex = 2 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1.22, 2))
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1.16, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 181.73, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 69.09, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 4.56, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 8.73, 2))
+            End If
+            'JPY'
+        ElseIf Com1.SelectedIndex = 3 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.0067, 2))
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.0064, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.0055, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.38, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.025, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.048, 2))
+            End If
+
+            'PHP'
+        ElseIf Com1.SelectedIndex = 4 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.018, 2))
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.017, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.014, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 2.63, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.066, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.13, 2))
+            End If
+            'SAR'
+        ElseIf Com1.SelectedIndex = 5 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.27, 2))
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.25, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.22, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 39.84, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 15.15, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1.91, 2))
+            End If
+
+            'CNY'
+        ElseIf Com1.SelectedIndex = 6 Then
+            If Com2.SelectedIndex = 0 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.14, 2))
+            ElseIf Com2.SelectedIndex = 1 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.13, 2))
+            ElseIf Com2.SelectedIndex = 2 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.11, 2))
+            ElseIf Com2.SelectedIndex = 3 Then
+                TextBox2.AppendText(Math.Round(Num1 * 20.82, 2))
+            ElseIf Com2.SelectedIndex = 4 Then
+                TextBox2.AppendText(Math.Round(Num1 * 7.92, 2))
+            ElseIf Com2.SelectedIndex = 5 Then
+                TextBox2.AppendText(Math.Round(Num1 * 0.52, 2))
+            ElseIf Com2.SelectedIndex = 6 Then
+                TextBox2.AppendText(Math.Round(Num1 * 1, 2))
+            End If
+        Else
+            MessageBox.Show("Select a currency!")
+        End If
+
+        If Num1 = 0 Then
+            TextBox2.Clear()
+        End If
+
+    End Sub
+
+    Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        Button1.BackColor = System.Drawing.Color.LightBlue
+    End Sub
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+        Button1.BackColor = System.Drawing.Color.Transparent
+    End Sub
+End Class
